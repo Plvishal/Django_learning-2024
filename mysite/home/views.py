6b03a1589a63601b90cb27963e1fb5b2308c5fd7
Vishal <vishalpal1501@gmail.com>
2024-05-26 13:34:17 +0530

    some degin

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     peoples=[
-        {"name":"Vishal","age":"23"},
-        {"name":"Ravi","age":"25"},
-        {"name":"Shivendra","age":"24"},
-        {"name":"Mangla","age":"25"},
-        {"name":"Akash","age":"22"},
-        {"name":"Krit","age":"24"},
-        {"name":"Risabh","age":"23"},
-        {"name":"Shailesh","age":"25"},
-        {"name":"Parul","age":"22"},
+        {"name":"Vishal","age":23},
+        {"name":"Ravi","age":25},
+        {"name":"Shivendra","age":24},
+        {"name":"Mangla","age":25},
+        {"name":"Akash","age":22},
+        {"name":"Krit","age":24},
+        {"name":"Risabh","age":23},
+        {"name":"Shailesh","age":25},
+        {"name":"Sonu","age":22},
+        {"name":"Harsha","age":14},
              
              ]
     return render(request,"index.html",context={'peoples':peoples})
