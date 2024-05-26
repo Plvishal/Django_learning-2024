842d094edf9aaf58ec78318420d4b710165cc00f
Vishal <vishalpal1501@gmail.com>
2024-05-26 12:50:40 +0530

    how to send data from backend to templates file like index in django

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    peoples=[
+        {"name":"Vishal","age":"23"},
+        {"name":"Ravi","age":"25"},
+        {"name":"Shivendra","age":"24"},
+        {"name":"Mangla","age":"25"},
+        {"name":"Akash","age":"22"},
+        {"name":"Krit","age":"24"},
+        {"name":"Risabh","age":"23"},
+        {"name":"Shailesh","age":"25"},
+        {"name":"Parul","age":"22"},
+             
+             ]
+    return render(request,"index.html",context={'peoples':peoples})
 
